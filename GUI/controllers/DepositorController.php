516311cd8c155d927c4aae55d6f2d84dad7e2d41
Alexander Zhigalin <alexander@zhigalin.tk>
2015-02-04 22:09:41 +0100

    typo fixed

--- GUI/controllers/DepositorController.php
+++ GUI/controllers/DepositorController.php
@@ -243,7 +243,7 @@ class DepositorController extends AbstractController {
 			  !empty($this->params['index']) &&
 			  !empty($this->params['email']) &&
 			  !empty($this->params['mobile']) &&
-			  !empty($this->params['state']) &&
+			  !empty($this->params['state'])
 			) {
 
 			$api_params
