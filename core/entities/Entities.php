48fe2d00f2a762f08c2c8aa078a2bc7da42d31f2
Alexander Zhigalin <alexander@zhigalin.tk>
2015-07-27 20:42:35 +0200

    Smarter entities structure

--- core/entities/Entities.php
+++ core/entities/Entities.php
@@ -1,5 +1,21 @@
 <?php
 
+class Entity {
+	protected $id;
+	
+	public function __construct() {
+		try {
+			DB::init();
+		} catch( Exception $e ) {
+			LogHandling::logDBError($e);
+			exit();
+		}
+	}
+	
+	public function bind($id) {
+	}
+}
+
 require '*.entity';
 
 ?>
