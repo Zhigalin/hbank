ab879cf2d08e56722016574b01a1f5f710b3e169
Alexander Zhigalin <alexander@zhigalin.tk>
2015-09-12 23:13:42 +0200

    Finished GUI transaction controller, finished core transaction check function

--- GUI/controllers/TransactionController.php
+++ GUI/controllers/TransactionController.php
@@ -17,12 +17,11 @@ class TransactionController extends AbstractController {
 			$data = $this->_API->sendRequest($api_params);
 		} catch( Exception $e ) {
 			#catch any exceptions and report the problem
-			@ErrorHandling::APIException('tr:verify', $data, $api_params, $e, $this->params);
+			@ErrorHandling::APIException('tr:do', $data, $api_params, $e, $this->params);
 		}
 		
-		if($mode === 'commit') {
-			LogHandling::logTransaction($from, $to, $hours, $data['result']);
-		}
+		#LogHandling::logTransaction($from, $to, $hours, $data['result'], $_SESSION['email']);
+		
 		return $data['result'];
 	}
 	
@@ -45,7 +44,38 @@ class TransactionController extends AbstractController {
 			exit();
 		}
 		
-		$code = $this->doTransaction('check', $this->params['from'], $this->params['to'], $this->params['hours']);
+		$code = $this->doTransaction('commit', $this->params['from'], $this->params['to'], $this->params['hours']);
+		
+		switch ($code) {
+			case 1:
+				$this->params['notification'] = $this->params['transaction_sender_less_than_5_hours_left_text'];
+				break;
+			case 2:
+				$this->params['notification'] = $this->params['transaction_sender_no_hours_left_text'];
+				break;
+			case 10:
+				$this->params['error'] = $this->params['transaction_sender_isnt_active_text'];
+				break;
+			case 11:
+				$this->params['error'] = $this->params['transaction_receiver_isnt_active_text'];
+				break;
+			case 12:
+				$this->params['error'] = $this->params['transaction_sender_hasnt_so_many_hours_text'];
+				break;
+			case 13:
+				$this->params['error'] = $this->params['transaction_sender_does_not_exist_text'];
+				break;
+			case 14:
+				$this->params['error'] = $this->params['transaction_receiver_does_not_exist_text'];
+				break;
+		}
+		
+		if($code >= 10) {
+			View::render('transaction_insert/error', $this->params);
+			exit();
+		}
+		
+		View::render('transaction_insert/success', $this->params);
 	}
 }
 ?>
--- core/controllers/TransactionController.php
+++ core/controllers/TransactionController.php
@@ -1,33 +1,82 @@
 <?php
 
-### Controller file for transactions related actions rev 1.0 ###
-
-class TransactionController
-{
-    private $_params;
- 
-    public function __construct($params)
-    {
-        $this->_params = $params;
-    }
- 
-    public function createAction()
-    {
-        #create a new transaction item
-    }
- 
-    public function readAction()
-    {
-        #read all the transaction items
-    }
- 
-    public function updateAction()
-    {
-        #update a transaction item
-    }
- 
-    public function deleteAction()
-    {
-        #delete a transaction item
-    }
+/**
+ * @brief Transaction related actions
+ */
+class TransactionController {
+	private $_params = array();
+	
+	public function __construct($params) {
+		$this->_params = $params;
+	}
+	
+/**
+ * @brief Check if a transaction is possible
+ * 
+ * @param int $from Transaction sender(ID)
+ * @param int $to Transaction receiver(ID)
+ * @param int $hours Number of hours to transfer
+ * @return int Return code(see documentation)
+ */
+	private function checkAction($from, $to, $hours) {
+		$toReturn  = 0;
+		
+		try {
+			$from = new DepositorEntity($from);
+		} catch(DepositorNotExistsException $e) {
+			return 13;
+		}
+		try {
+			$to = new DepositorEntity($to);
+		} catch(DepositorNotExistsException $e) {
+			return 14;
+		}
+		
+		if (!$to->bind()) {
+			
+		}
+		
+		$deltaHours = $from->getHours()-$hours;
+		if($deltaHours < 5) {
+			$toReturn = 1;
+		}
+		if($deltaHours = 0) {
+			$toReturn = 2;
+		} elseif($deltaHours < 0) {
+			return 12;
+		}
+		
+		if(!$from->isActive()) {
+			return 10;
+		}
+		if(!$to->isActive()){
+			return 11;
+		}
+		
+		return $toReturn;
+	}
+	
+/**
+ * @brief Verity and make the transaction
+ * 
+ * @return int Result code
+ */
+	public function commitAction() {
+		$code = $this->checkAction($this->_params['from'], $this->_params['to'], $this->_params['hours']);
+		if($code >= 10) {
+			return $code;
+		}
+	}
+	
+	public function readAction() {
+		#read all the transaction items
+	}
+	
+	public function updateAction() {
+		#update a transaction item
+	}
+	
+	public function deleteAction() {
+		#delete a transaction item
+	}
 }
